3ded6c0c208f1021218f9df05d9d1ea98a685652
rdelvallej32 <rdelvallej@gmail.com>
2016-04-27 13:27:39 -0400

    response to diagnostic

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :person
+  has_many :cities, through: :addresses
 end
--- diagnostic.rb
+++ diagnostic.rb
@@ -1,9 +1,40 @@
 # Question 1. Paste your code below.
-
+'db/migrate/20160427170541_create_pets.rb'
+'db/migrate/20160427170616_add_personto_pets.rb'
 # Question 2. Paste your code below.
+class Pet < ActiveRecord::Base
+  belongs_to :person, inverse_of: :pets
+end
+
+class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :person
+end
 
 # Question 3. Paste your code below.
 
+sam = Pet.first
+mam = Pet.last
+
+jonah = Person.first
+
+jonah << sam
+jonah << mam
+
 # Question 4. Paste your code below.
+rails g model address person:references city:references
 
 # Question 5. Paste your code below.
+
+class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :person
+  has_many :cities, through: :addresses
+end
+
+class City < ActiveRecord::Base
+  has_many :people, through: :addresses
+end
+
+class Address < ActiveRecord::Base
+  belongs_to :person
+  belongs_to :city
+end
