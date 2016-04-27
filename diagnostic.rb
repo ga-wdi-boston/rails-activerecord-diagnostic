ee1caf3332385cb618d468e64aaf8906ff8f762e
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2016-04-27 13:29:02 -0400

    Add answers

--- diagnostic.rb
+++ diagnostic.rb
@@ -1,8 +1,22 @@
 # Question 1. Paste your code below.
 
+rails g migration AddPersonToPets people:references
+
 # Question 2. Paste your code below.
+class Person < ActiveRecord::Base
+  has_many :pets
+end
+
+class Pet < ActiveRecord::Base
+  belongs_to :person, inverse_of: :pets
+end
 
 # Question 3. Paste your code below.
+
+Jim = Person.first
+Tom = Pet.first
+Jim.pets << Tom
+
 
 # Question 4. Paste your code below.
 
