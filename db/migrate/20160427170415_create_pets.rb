ffc6d99ee7ff39520f21bca9234769f854b5f5e1
olivia-leach <olivia.i.m.leach@gmail.com>
2016-04-27 13:17:03 -0400

    Respond to diagnostic questions

---
+++ db/migrate/20160427170415_create_pets.rb
@@ -0,0 +1,13 @@
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.date :born_on
+      t.string :kind
+      t.string :name
+
+      t.references :person, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- diagnostic.rb
+++ diagnostic.rb
@@ -1,9 +1,55 @@
 # Question 1. Paste your code below.
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.date :born_on
+      t.string :kind
+      t.string :name
+
+      t.references :person, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
 
 # Question 2. Paste your code below.
+class Pet < ActiveRecord::Base
+  belongs_to :person, inverse_of: :pets
+end
+
+class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :person
+end
 
 # Question 3. Paste your code below.
+Person.first.pets << Pet.first << Pet.last
 
 # Question 4. Paste your code below.
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.references :person, index: true, foreign_key: true
+      t.references :city, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
 
 # Question 5. Paste your code below.
+class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :person
+  has_many :cities, through: :addresses
+  has_many :addresses
+end
+
+class City < ActiveRecord::Base
+  has_many :people, through: :addresses
+  has_many :addresses
+end
+
+class Address < ActiveRecord::Base
+  belongs_to :person, inverse_of: :addresses
+  belongs_to :city, inverse_of: :addresses
+end
