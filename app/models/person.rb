7436e568102f0fe9c104de747e57adf4553668c0
V4Vince <vince.guoo@gmail.com>
2016-04-27 13:29:40 -0400

    ansewers

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,3 @@
 class Person < ActiveRecord::Base
+  has_many :pets, inverse_of: :owner, foreign_key: 'owner_id'
 end
--- diagnostic.rb
+++ diagnostic.rb
@@ -1,9 +1,30 @@
 # Question 1. Paste your code below.
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.string :born_on
+      t.string :kind
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
 
 # Question 2. Paste your code below.
+class Person < ActiveRecord::Base
+  has_many :pets
+end
+
+class Pet < ActiveRecord::Base
+  belongs_to :person, inverse_of: :pets
+end
 
 # Question 3. Paste your code below.
 
 # Question 4. Paste your code below.
-
+class Address < ActiveRecord::Base
+  belongs_to :person, inverse_of: :addresses
+  belongs_to :city, inverse_of: :addresses
+end
 # Question 5. Paste your code below.
